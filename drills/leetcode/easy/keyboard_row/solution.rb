8aa2d32e314571378e3b477f41f87e872774733d
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-03-15 12:27:19 +0200

    Create solution.rb

---
+++ drills/leetcode/easy/keyboard_row/solution.rb
@@ -0,0 +1,23 @@
+# @param {String[]} words
+# @return {String[]}
+def find_words(words)
+  require 'set'
+  
+  kb_rows = [
+    Set.new("QWERTYUIOPqwertyuiop".chars),
+    Set.new("ASDFGHJKLasdfghjkl"  .chars),
+    Set.new("ZXCVBNMzxcvbnm"      .chars),
+  ]
+  
+  return words.find_all { |w|
+    if w.size > 0
+      kb_row = kb_rows.detect { |x|
+        x.include?(w[0])
+      }
+      
+      kb_row && w.chars.all? { |c|
+        kb_row.include?(c)
+      }      
+    end
+  }  
+end
