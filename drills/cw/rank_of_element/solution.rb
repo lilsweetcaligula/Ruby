19358726688c26221ffb2ff565bcb1af3ab3c100
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-03-15 08:36:35 +0200

    Create solution.rb

---
+++ drills/cw/rank_of_element/solution.rb
@@ -0,0 +1,7 @@
+def rank_of_element(arr, i)
+  indices = (0...arr.size).to_a.sort_by! { |x| arr[x] }  
+  lteqcnt = indices.count { |x| x < i && arr[x] <= arr[i] }
+  gtcnt   = indices.count { |x| x > i && arr[x] <  arr[i] }
+  
+  return lteqcnt + gtcnt
+end
