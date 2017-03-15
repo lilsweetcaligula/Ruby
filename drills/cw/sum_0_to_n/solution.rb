8905523643435a6b6237ebc4741163d9d91c09d9
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-03-15 08:31:45 +0200

    Create solution.rb

---
+++ drills/cw/sum_0_to_n/solution.rb
@@ -0,0 +1,16 @@
+class SequenceSum
+  def self.show_sequence(n)
+    if n.negative?
+        return "#{n}<0"
+    end
+    
+    if n.zero?
+      return "#{n}=0"
+    end
+  
+    vals = (0..n).to_a
+    res  = vals.reduce(:+)
+  
+    return "#{vals.join("+")} = #{res}"
+  end
+end
