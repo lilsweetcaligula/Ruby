ae9644824c35c6b21e59b9604651dfd990eef3b2
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2017-03-16 05:35:45 +0200

    Create solution.rb

---
+++ drills/16_march_2017/leetcode/easy/reverse_string_ii/solution.rb
@@ -0,0 +1,15 @@
+# @param {String} s
+# @param {Integer} k
+# @return {String}
+def reverse_str(s, k)
+  s.chars
+   .each_slice(k)
+   .map.with_index() { |chunk, i| 
+     if i % 2 == 0 
+       chunk.reverse
+     else 
+       chunk
+     end 
+   }.flatten
+    .join
+end
